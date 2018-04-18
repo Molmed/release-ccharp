a68460b648544fc70e5352ba4920c66f1a4ba3e3
GitEdvard <edvard.englund@medsci.uu.se>
2018-04-18 11:06:51 +0200

    Rebase config
    Move imported config files e.g. from Chiasma and SQAT to directory
    Resources. Rebase config.py, add internal method to read from file, and
    internally call this method instead of repeating code. Add config files
    for FP.

--- tests/unit/utility/config.py
+++ tests/unit/utility/config.py
@@ -2,14 +2,25 @@ import os
 
 
 def load_chiasma_config():
-    here = os.path.dirname(__file__)
-    with open(os.path.join(here, 'chiasma.exe.config'), 'r') as f:
-        content = f.read()
-    return content
+    return _load_file('chiasma.exe.config')
+
 
 def load_sqat_connect():
+    return _load_file('SQATconnect.xml')
+
+
+def load_fp_config():
+    return _load_file('FPDatabase.exe.config')
+
+
+def load_fp_connect_config():
+    return _load_file('FPDatabaseConfig.txt')
+
+
+def _load_file(file_name):
     here = os.path.dirname(__file__)
-    with open(os.path.join(here, 'SQATconnect.xml'), 'r') as f:
+    resources = os.path.join(here, 'resources')
+    with open(os.path.join(resources, file_name), 'r') as f:
         content = f.read()
     return content
 
@@ -17,3 +28,7 @@ def load_sqat_connect():
 CHIASMA_CONFIG = load_chiasma_config()
 
 SQAT_CONNECT = load_sqat_connect()
+
+FP_CONFIG = load_fp_config()
+
+FP_CONNECT_CONFIG = load_fp_connect_config()
