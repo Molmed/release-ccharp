278526a13a7c1b6f4e214242e73d2bc770ba1a9d
Edvard Englund <edvard.englund@medsci.uu.se>
2019-08-29 16:02:59 +0200

    Replace outdated fake_shutil_module methods
    Replace by home made methods. This fixes the tests.

--- tests/unit/utility/fake_os_service.py
+++ tests/unit/utility/fake_os_service.py
@@ -5,6 +5,7 @@ from pyfakefs.fake_filesystem import FakeFileOpen
 from xml.etree import ElementTree as ET
 from xml.etree.ElementTree import ElementTree
 from contextlib import contextmanager
+from release_ccharp.utils import copytree_replace_existing
 
 
 class FakeOsService:
@@ -23,10 +24,15 @@ class FakeOsService:
         return self.os_module.path.isfile(path)
 
     def copytree(self, src, dst):
-        self.shutil_module.copytree(src, dst)
+        copytree_replace_existing(self, src, dst)
 
     def copyfile(self, src, dst):
-        self.shutil_module.copyfile(src, dst)
+        if self.exists(dst):
+            self.remove_file(dst)
+        fake_open = FakeFileOpen(self.filesystem)
+        with fake_open(src) as f:
+            contents = ''.join([line for line in f])
+        self.filesystem.create_file(dst, contents=contents)
 
     def exists(self, path):
         return self.os_module.path.exists(path)
@@ -74,4 +80,4 @@ class FakeOsService:
         self.os_module.unlink(path)
 
     def rmtree(self, path):
-        self.shutil_module.rmtree(path)
+        self.filesystem.remove_object(path)
